d3320f71f2a5a529e0262a33447aeb19717943b6
Vinicius de Oliveira <viniolvs.dev@gmail.com>
2024-05-30 14:43:53 -0300

    add: api setup

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para parsear JSON
+app.use(bodyParser.json());
+app.use(cors());
+
+// Rotas
+app.use("/", require("./routes"));
+
+// Conexão ao MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => {
+    console.log(err)
+    process.exit(1)
+  });
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
